37d079a52b574703997ce611de2a09fc994fdb72
Don Sagrott <don.sagrott@gmail.com>
2019-10-21 12:42:14 -0700

    UPDATE-FEATURE-Authorization -v-002 - Changed functionality of 'Articles Index' to display 'Editing toolbar' to only the Author of the article.

---
+++ Data/Migrations/20191021164343_add-feild-authorId-to-table-Articles.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnetThree.Data.Migrations
+{
+    public partial class addfeildauthorIdtotableArticles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Article",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Article");
+        }
+    }
+}
